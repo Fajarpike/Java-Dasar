a98ca2700bd4efab0aaf419354863ec500fc8cb3
fajarpike <fpikekah@gmail.com>
2023-02-20 10:31:06 +0700

    menambahkan class DataKaryawan

--- Geeks/src/Karyawan.java
+++ Geeks/src/Karyawan.java
@@ -1,3 +1,36 @@
+
+class DataKaryawan {
+
+    public String Nik;
+    public String Divisi;
+    public String Jabatan;
+
+    DataKaryawan(String noNik, String Div, String Jabatan){
+        this.Nik = noNik;
+        this.Divisi = Div;
+        this.Jabatan = Jabatan;
+    }
+
+    public String getNik() {
+        return Nik;
+    }
+
+    public String getDivisi() {
+        return Divisi;
+    }
+
+    public String getJabatan() {
+        return Jabatan;
+    }
+
+    public String toString() {
+
+        return ("Nik Karyawan " + this.getNik() + "\n" + "Divisi Karyawan : " + this.getDivisi() + "\n"
+        + "Jabatan Karyawan : " + this.getJabatan());
+    }
+
+}
+
 public class Karyawan {
 
     //declare variable
@@ -21,5 +54,11 @@ public class Karyawan {
         Karyawan result = new Karyawan();
         result.setKaryawan("Fajar", 9000000);
         result.getKaryawan();
+
+        //get class DataKaryawan
+        DataKaryawan datakaryawan = new DataKaryawan("018097", "IT Middleware", "Section Head");
+        String data = datakaryawan.toString();
+        System.out.println(data);
     }
+
 }
